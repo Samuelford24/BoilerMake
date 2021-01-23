a8c3cf028c72e0ffceaa04d08f9349b330d26501
Samuelford24 <Samuelford48@gmail.com>
2021-01-22 21:52:57 -0500

    initial layout

--- app/src/main/java/com/sf/boilermake/ui/SettingsFragment.java
+++ app/src/main/java/com/sf/boilermake/ui/SettingsFragment.java
@@ -1,10 +1,6 @@
-package com.sf.boilermake.ui.home;
+package com.sf.boilermake.ui;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,16 +8,25 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
 import com.sf.boilermake.R;
 
-public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
+/**
+ * A simple {@link Fragment} subclass.
+
+ * create an instance of this fragment.
+ */
+public class SettingsFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        View root = inflater.inflate(R.layout.fragment_settings, container, false);
 
         return root;
     }
